26849e4ca89a21b3303393eef919f71f342e0645
Artem <shyposhartem.biz@gmail.com>
2024-11-30 21:22:08 +0200

    slider_complate

---
+++ project2/js/app.js
@@ -0,0 +1,11 @@
+new Swiper('.slider', {
+    speed : 2400,
+    direction: 'vertical',
+    mousewheel: {
+        enabled: true,
+        sensitivity: 3
+    },
+    spaceBetween: 18,
+    parallax: true,
+    freeMode: true,
+})
